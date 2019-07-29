0b507efa691a0133af4fb64fea19f0a11f5cdc12
Carlo Battilana <carlo.battilana@gmail.com>
2019-07-29 19:24:59 +0200

    First version of crab cfg

---
+++ production/crab_dtDpgNtuples_slicetest_cfg.py
@@ -0,0 +1,46 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+##### Configuration parameters ################################
+
+runNumber = 330161
+inputDataset = "/Cosmics/Commissioning2019-v1/RAW"
+
+# These are the cfg parameters used to configure the 
+# dtDpgNtuples_slicetest_cfg.py configuration file
+configParams = ['ntupleName=DTDPGNtuple.root'] 
+# E.g. use dedicated tTrigs
+# configParams = ['ntupleName=DTDPGNtuple.root', \
+#                 'tTrigFile=calib/TTrigDB_cosmics_ttrig.db'] 
+
+# These are the additional input files (e.g. sqlite .db) 
+# needed by dtDpgNtuples_slicetest_cfg.py to run
+inputFiles = [] 
+# E.g. use dedicated tTrigs
+# inputFiles = ['./calib/TTrigDB_cosmics_ttrig.db'] 
+
+###############################################################
+
+config.section_('General')
+config.General.workArea = 'crab_jobs'
+config.General.requestName = 'DTDPGNtuples_SliceTest_run' + str(runNumber)
+config.General.transferOutputs = True
+
+config.section_('JobType')
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'dtDpgNtuples_slicetest_cfg.py'
+
+config.JobType.pyCfgParams = configParams
+config.JobType.inputFiles  = inputFiles
+
+config.section_('Data')
+config.Data.inputDataset = inputDataset
+
+config.Data.splitting    = 'LumiBased'
+config.Data.unitsPerJob  = 50  
+config.Data.inputDBS     = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.outLFNDirBase  = '/store/group/dpg_dt/comm_dt/commissioning_2019_data/crab/'
+
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+
