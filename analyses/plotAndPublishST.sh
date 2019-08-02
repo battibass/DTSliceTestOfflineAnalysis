206bbdd67019e9f6e6cf929c81787d0d1518b91c
Carlo Battilana <carlo.battilana@gmail.com>
2019-08-02 16:51:23 +0200

    Copy to EOS enabled again

--- analyses/plotAndPublishST.sh
+++ analyses/plotAndPublishST.sh
@@ -6,7 +6,7 @@
 
 UTILS_FOLDER="./plotAndPublish/"
 PUBLUSH_FOLDER="/eos/project/c/cmsweb/www/MUON/dpgdt/sx5/Results/SliceTest"
-# PUBLUSH_FOLDER="/eos/user/b/battilan/www/DTDPG/proveSliceTest/"
+# PUBLUSH_FOLDER="/eos/user/b/battilan/www/DTDPG/SliceTest/"
 #PUBLUSH_FOLDER="/afs/cern.ch/user/g/gpinnaan/www/DTslice/ProveSliceTest"
 
 PLOTTER_CFGS=("configDigiST.json" "configTriggerST.json" "configSegmentST.json")
@@ -38,4 +38,4 @@ done
 python $UTILS_FOLDER/$PUBLISH_CMD $RUN_FOLDER $UTILS_FOLDER/$INDEX_FILE
 
 # scp -r $RUN_FOLDER dtdqm@lxplus:$PUBLUSH_FOLDER
-#cp -r $RUN_FOLDER $PUBLUSH_FOLDER
+cp -r $RUN_FOLDER $PUBLUSH_FOLDER
