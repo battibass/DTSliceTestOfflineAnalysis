0815ab6a92dc92e732e6991715e870fa35527a09
Carlo Battilana <carlo.battilana@gmail.com>
2020-09-15 18:18:39 +0200

    Add loadEvDisp.C

---
+++ analyses/loadEvDisp.C
@@ -0,0 +1,12 @@
+#include "TROOT.h"
+#include "TString.h"
+
+void loadAllAnalyses()
+{
+
+  gROOT->ProcessLine(".L DTNtupleBaseAnalyzer.C++");
+  gROOT->ProcessLine(".L DTNtupleDigiAnalyzer.C++");
+  gROOT->ProcessLine(".L DTNtupleTriggerAnalyzer.C++");
+  gROOT->ProcessLine(".L DTNtupleSegmentAnalyzer.C++");
+
+}
