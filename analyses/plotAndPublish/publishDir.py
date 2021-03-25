d2dce0c9354d62046e8ca887e638a38d9b448120
Carlo Battilana <carlo.battilana@cern.ch>
2021-03-25 13:07:45 +0100

    Pylint compliant

--- analyses/plotAndPublish/publishDir.py
+++ analyses/plotAndPublish/publishDir.py
@@ -2,7 +2,11 @@
 # pylint: disable=C0103
 
 """
-Add php formatting file recursively to a given folder.
+This program goes from the toplevel directory recursively downwards and
+places a index PHP in each directory.
+Then, it's possible to browse files more conveniently with a web browser.
+Note, that the PHP files have to be accessed through a web server such
+as Apache with PHP support.
 """
 
 import argparse
@@ -12,14 +16,7 @@ import os
 # Setup argument parser
 #########################
 
-parser = argparse.ArgumentParser(description="This program goes from the toplevel \
-                                              directory recursively downwards and \
-                                              places a index PHP in each directory. \
-                                              Then, it's possible to browse files \
-                                              more conveniently with a web browser. \
-                                              Note, that the PHP files have to be \
-                                              accessed through a web server such \
-                                              as Apache with PHP support.")
+parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("toplevelDirectory", help="Path to the toplevel directory, which \
                                                is the starting point for the recursion")
 parser.add_argument("inputPHPfile", help="Path to the file source, which is placed \
