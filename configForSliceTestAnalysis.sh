2e581b7a1dfb14e5509525378ecf9396c27085aa
Carlo Battilana <carlo.battilana@gmail.com>
2021-04-01 10:41:29 +0200

    fix typo

--- configForSliceTestAnalysis.sh
+++ configForSliceTestAnalysis.sh
@@ -1,7 +1,7 @@
 CMSSW_FOLDER=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DT/OfflineCode/SliceTest/v9p7p1/CMSSW_11_2_3/
 DT_EOS_FOLDER=/eos/cms/store/group/dpg_dt/comm_dt/commissioning_2021_data/ntuples/
 TTRIG_DATASET=/eos/cms/store/data/Commissioning2021/MiniDaq/RAW/v1/
-CRAB_CONFIG_SCRIPT=/cvmfs/cms.cern.ch/crab3/crab.csh
+CRAB_CONFIG_SCRIPT=/cvmfs/cms.cern.ch/crab3/crab.sh
  
 cd $CMSSW_FOLDER/src
 echo "[$0]: Configuring CMSSW from directory $CMSSW_FOLDER/src"
