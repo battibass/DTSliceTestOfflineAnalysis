416fcad20fe6f3a599d295218baedd374106baee
Carlo Battilana <carlo.battilana@gmail.com>
2021-04-22 15:23:54 +0200

    First complete version of scripts to handle ntuples produced using CRAB -> adding crab_json_to_xml.py

---
+++ production/crab_json_to_xml.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""
+This program generates an XML file to be processed by 
+the offline DTSliceTestOfflineAnalysis workflow which 
+combines information from the JSON file produced for 
+ntuple production with CRAB and the actual location of 
+the (skimmed and merged) DTDPGNtuple
+"""
+
+import lxml.etree as etree
+import argparse
+import json
+import os
+
+#----------------
+# Variables
+#----------------
+
+OUT_FOLDER = "crab_xml"
+
+if __name__ == '__main__':
+
+    #----------------------
+    # Setup argument parser
+    #----------------------
+
+    PARSER = argparse.ArgumentParser(description=__doc__)
+
+    PARSER.add_argument("crabJSONFile",
+                        help="JSON file used for processing with CRAB")
+
+    PARSER.add_argument("dtNtuple",
+                        help="The (skimmed and merged) DTNtuple after CRAB processing")
+
+    ARGS = PARSER.parse_args()
+
+    #---------------------------------
+    # Generate XML configuration file
+    #---------------------------------
+    
+    if not os.path.isfile(ARGS.crabJSONFile):
+        print("[{}] file : {} does not exist. Quitting".format(__file__, ARGS.crabJSONFile))
+        exit(100)
+
+    if not os.path.isfile(ARGS.dtNtuple):
+        print("[{}] file : {} does not exist. Quitting".format(__file__, ARGS.dtNtuple))
+        exit(100)
+
+    # READ and parse JSON
+    with open(ARGS.crabJSONFile) as json_file:
+        JSON = json.load(json_file)
+
+    # default params
+    PARAMS = {"t0File" : "",
+              "t0FilePh2" : "",
+              "tTrigFile" : "",
+              "tTrigFilePh2" : "",
+              "vDriftFile" : "",
+              "inputFolderCentral" : "dataset {}".format(JSON["dataset"]), 
+              "inputFolderDT" : "NONE", 
+              "globalTag" : "NONE", 
+              "runNumber" : JSON["runNumber"], 
+              "ntupleName" : os.path.abspath(ARGS.dtNtuple), 
+              "nEvents" : "-1", 
+              "runOnTestPulse" : "False", 
+              "runOnDat" : "False", 
+              "inputFile" : "NONE"}
+
+    for param_string in JSON["config_params"]:
+
+        var, param = param_string.split("=")
+
+        if param == "ntupleName":
+            continue
+
+        for path in JSON["input_files"]:
+            if param in path:
+                PARAMS[var] = path
+
+    # fill and write XML
+    XML_BASE = etree.Element("options") 
+
+    for var, param in PARAMS.items():
+        etree.SubElement(XML_BASE, var).text = param
+
+    if not os.path.exists(OUT_FOLDER):
+        os.makedirs(OUT_FOLDER)
+
+    xml_string = etree.tostring(XML_BASE, pretty_print=True)
+
+    out_file_name = "ntuple_cfg_run" + PARAMS["runNumber"] + ".xml"
+    out_file_path = os.path.join(OUT_FOLDER, out_file_name)
+
+    with open(out_file_path, 'w') as file: file.write(xml_string)
